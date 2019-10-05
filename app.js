b7d796c24018e544be0500b5e4c2ceedfeb6780f
jayjscotto <jayjscotto@gmail.com>
2019-10-05 12:45:45 -0400

    added counting function example PR

--- app.js
+++ app.js
@@ -2,3 +2,13 @@
 
 
 console.log("hello World!!!")
+
+//counts from one up to ten
+function count() {
+    for (let i = 0; i < 10; i ++) {
+        console.log(i);
+    }
+} 
+
+//call count function
+count();
