231a3d622350f7f98f1a585e8f090ec0fc3ea94d
Wismy Seide <wismyseide@outlook.com>
2019-10-12 10:52:23 -0400

    commit

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
-=======
-//comment
-=======
 $(document).ready(function() {
     $("#stay-in").hide();
     $("#go-out").hide();
->>>>>>> 21859403fa56301618071b9b07ae345ed06c7fab
+
 
     $("#btn-in").on("click", function(e){
         e.preventDefault();
@@ -131,25 +113,21 @@ $(document).ready(function() {
                     //modal to explain quick search?
                 }
     });
+
+    //accordion code
+    var acc = document.getElementsByClassName("accordion");
+
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          } 
+        });
+      }
 })
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-//counts from one up to ten
-function count() {
-    for (let i = 0; i < 10; i ++) {
-        console.log(i);
-    }
-} 
-
-//call count function
-count();
->>>>>>> bd20ca846f5123479e330295981d9edbd0fd6787
-=======
->>>>>>> 21859403fa56301618071b9b07ae345ed06c7fab
-=======
-
-
-
->>>>>>> fd1fe20911fb5034f0068ef2c1a0d0740f4e0b9a
